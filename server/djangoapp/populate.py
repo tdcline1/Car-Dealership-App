c5d0008aaef46d118649977d784d37d8623b3c77
Thomas Cline <149266783+tdcline1@users.noreply.github.com>
2025-02-04 11:19:45 -0500

    Update populate.py for linting

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -113,11 +113,11 @@ car_model_data = [
     },
 ]
 
-    for data in car_model_data:
-            CarModel.objects.create(
-                name=data['name'], 
-                car_make=data['car_make'], 
-                type=data['type'], 
-                year=data['year']
-            )
+for data in car_model_data:
+    CarModel.objects.create(
+        name=data['name'], 
+        car_make=data['car_make'], 
+        type=data['type'], 
+        year=data['year']
+    )
         
