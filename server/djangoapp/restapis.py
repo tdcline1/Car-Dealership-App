3c1094de6eb8121a6662bb0c2da0dcb9643be870
Thomas Cline <149266783+tdcline1@users.noreply.github.com>
2025-02-04 11:48:38 -0500

    Update restapis.py for linting

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -24,9 +24,8 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
-        # If any error occurs
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def analyze_review_sentiments(text):
@@ -46,5 +45,5 @@ def post_review(data_dict):
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"Error: {e}")
