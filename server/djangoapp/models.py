f46fd9ec67fe69ad7881e6a1e5577acdef7b9d8d
Thomas Cline <149266783+tdcline1@users.noreply.github.com>
2025-02-04 11:18:28 -0500

    Update models.py for linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,8 +5,6 @@ from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
-
 # Create a Car Make model
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -28,12 +26,14 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     # - Year (IntegerField) with min value 2015 and max value 2023
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ]
+            MinValueValidator(2015),
+        ],
     )
+
 
     def __str__(self):
         return self.name  # Return the name as the string representation
