94cd23840cb2dc9c7a75a571f21aac266f780ade
Thomas Cline <149266783+tdcline1@users.noreply.github.com>
2025-02-04 11:11:24 -0500

    Update views.py for linting

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -26,14 +26,14 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, 
+        cars.append({"CarModel": car_model.name,
                      "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
-    
+
     # Get username and password from request.POST dictionary
     data = json.loads(request.body)
     username = data['userName']
@@ -49,7 +49,7 @@ def login_user(request):
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
-    
+
     logout(request)
     data = {"userName": ""}
     return JsonResponse(data)
@@ -57,7 +57,7 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    
+
     context = {}
     data = json.loads(request.body)
     username = data['userName']
@@ -77,23 +77,23 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username= username, 
-                                        first_name= first_name, 
-                                        last_name= last_name,
-                                        password= password, 
-                                        email= email
+        user = User.objects.create_user(username=username,
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password,
+                                        email=email
                                        )
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
+    else:
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
 # render list of all dealerships by default, by state if passed
 def get_dealerships(request, state="All"):
-    
+
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
@@ -103,7 +103,7 @@ def get_dealerships(request, state="All"):
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
-    
+
     # if dealer id has been provided
     if (dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
@@ -118,24 +118,24 @@ def get_dealer_reviews(request, dealer_id):
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
-    
-    if(dealer_id):
+
+    if (dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
-        return JsonResponse({"status":200,"dealer":dealership})
+        return JsonResponse({"status": 200, "dealer": dealership})
     else:
-        return JsonResponse({"status":400,"message":"Bad Request"})
+        return JsonResponse({"status": 400, "message": "Bad Request"})
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    
+
     if (request.user.is_anonymous == False):
         data = json.loads(request.body)
         try:
             response = post_review(data)
             return JsonResponse({"status": 200})
         except:
-            return JsonResponse({"status": 401, 
+            return JsonResponse({"status": 401,
                                  "message": "Error in posting review"}
                                )
     else:
