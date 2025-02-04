283d64d0265beca7ef4dfeb4578f3ab7ac4b1c39
Thomas Cline <149266783+tdcline1@users.noreply.github.com>
2025-02-04 10:28:47 -0500

    Update models.py for linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -7,7 +7,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
-# Create a Car Make model 
+# Create a Car Make model
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -17,8 +17,9 @@ class CarMake(models.Model):
 
 # Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
-    # - Many-To-One relationship to Car Make model (One Car Make has manyCar Models, using ForeignKey field)
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    
+    # Many-To-One relationship to Car Make
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -31,7 +32,8 @@ class CarModel(models.Model):
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
+        ]
+    )
 
     def __str__(self):
         return self.name  # Return the name as the string representation
