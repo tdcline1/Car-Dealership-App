1423afe3dd52aa5e0cecde1bfc5982ae7daf8314
Thomas Cline <149266783+tdcline1@users.noreply.github.com>
2025-02-04 10:42:01 -0500

    Update restapis.py for linting

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,10 +11,11 @@ sentiment_analyzer_url = os.getenv(
     default="http://localhost:5050/")
 
 def get_request(endpoint, **kwargs):
+    
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params= params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -28,6 +29,7 @@ def get_request(endpoint, **kwargs):
         print("Network exception occurred")
 
 def analyze_review_sentiments(text):
+    
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
@@ -38,9 +40,10 @@ def analyze_review_sentiments(text):
         print("Network exception occurred")
 
 def post_review(data_dict):
+    
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
