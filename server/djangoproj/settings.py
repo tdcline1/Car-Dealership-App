d1dbda5be9ad9ef6b6b37cf76592228a30312166
Thomas Cline <149266783+tdcline1@users.noreply.github.com>
2025-02-13 22:32:07 +0000

    CORS: install django-cors-headers, add to requirements and add to settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -37,6 +37,8 @@ CSRF_TRUSTED_ORIGINS = [
     ),
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
@@ -51,9 +53,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
