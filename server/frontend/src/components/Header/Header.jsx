03be9b87406b247ae0d62d46cb6fa4cc790d4b33
Thomas Cline <149266783+tdcline1@users.noreply.github.com>
2025-02-14 02:22:52 +0000

    header navigation

--- server/frontend/src/components/Header/Header.jsx
+++ server/frontend/src/components/Header/Header.jsx
@@ -49,13 +49,13 @@ if ( curr_user !== null &&  curr_user !== "") {
               <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a class="nav-link active" style={{fontSize: "larger"}} aria-current="page" href={'${BACKEND_URL}'}>Home</a>
+                    <a class="nav-link active" style={{fontSize: "larger"}} aria-current="page" href={BACKEND_URL}>Home</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" style={{fontSize: "larger"}} href={'${BACKEND_URL}/about'}>About Us</a>
+                    <a class="nav-link" style={{fontSize: "larger"}} href={`${BACKEND_URL}/about`}>About Us</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" style={{fontSize: "larger"}} href={'${BACKEND_URL}/contact'}>Contact Us</a>
+                    <a class="nav-link" style={{fontSize: "larger"}} href={`${BACKEND_URL}/contact`}>Contact Us</a>
                   </li>
                 </ul>
                 <span class="navbar-text">
